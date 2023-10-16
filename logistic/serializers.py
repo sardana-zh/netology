74255cd6c3f69cb8f787247241bfce532ea461da
sardana-zh <arjakova87@mail.ru>
2023-10-16 23:27:48 +0900

    stocks_products

---
+++ logistic/serializers.py
@@ -0,0 +1,39 @@
+from rest_framework import serializers
+from .models import Product, StockProduct, Stock
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ['id','title', 'description']
+
+class ProductPositionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = StockProduct
+        fields = ['product','quantity','price']
+
+class StockSerializer(serializers.ModelSerializer):
+    positions = ProductPositionSerializer(many=True)
+
+    class Meta:
+        model = Stock
+        fields = ['address','positions']
+
+    def create(self, validated_data):
+        positions = validated_data.pop('positions')
+
+        stock = super().create(validated_data)
+
+        for position in positions:
+            StockProduct.objects.create(stock=stock, **position)
+
+        return stock
+
+    def update(self, instance, validated_data):
+        positions = validated_data.pop('positions')
+
+        stock = super().update(instance, validated_data)
+
+        for position in positions:
+            StockProduct.objects.update_or_create(stock=stock, **position)
+
+        return stock
